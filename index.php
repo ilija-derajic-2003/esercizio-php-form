f53dc956e95b73baec36a5561861d62550a01936
ilija-derajic-2003 <derajicilija78@gmail.com>
2019-03-18 11:15:23 +0100

    aggiunto nuovo codice php

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
+<?php
+	$nome = (isset($_POST['nome'])) ? $_POST['nome']:'';
+	$cognome = (isset($_POST['cognome'])) ? $_POST['cognome']:'';
+	$indirizzo = (isset($_POST['indirizzo'])) ? $_POST['indirizzo']:'';
+	
+?>
 <!doctype html>
-<html lang="en">
+<html lang="it">
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
@@ -12,27 +18,36 @@
   </head>
   <body>
     <div class="container">
-	  	<form>
+    <div class="alert alert-primary" role="alert">
+    	<?php
+	    	print $nome . '<br>';
+				print $cognome . '<br>';
+				print $indirizzo . '<br>';
+			?>
+		</div>
+	  	<form action="index.php" method="post">
 	  	  <div class="form-group">
 			    <label for="nome">Nome</label>
-			    <input type="text" class="form-control" id="nome" placeholder="Inserisci il tuo nome">
+			    <input type="text" name="nome" class="form-control" id="nome" placeholder="Inserisci il tuo nome">
 	  		</div>
 
 	  		<div class="form-group">
 			    <label for="Cognome">Cognome</label>
-			    <input type="text" class="form-control" id="Cognome" placeholder="Inserisci il tuo Cognome">
+			    <input type="text" name="Cognome" class="form-control" id="Cognome" placeholder="Inserisci il tuo Cognome">
 	  		</div>
 	  	
 	   		<div class="form-group">
 			    <label for="indirizzo">indirizzo</label>
-			    <textarea class="form-control" id="indirizzo" rows="3"></textarea>
+			    <textarea class="form-control" name="indirizzo" id="indirizzo" rows="3"></textarea>
 	  		</div>
 
-	  		<button type="submit" class="btn btn-primary">invio</button>
+	  		<button type="submit" class="btn btn-primary">invia</button>
 			</form>
+
   	</div>
 
-    <!-- Optional JavaScript -->
+
+    <!-- Optional JavaScript --
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="js/jquery-3.3.1.min.js"></script>
     <script src="js/popper.min.js"></script>
